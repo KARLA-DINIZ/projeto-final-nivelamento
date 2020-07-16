f1960b483efe296922db904c26a795d4dbe776e6
KARLA-DINIZ <68313598+KARLA-DINIZ@users.noreply.github.com>
2020-07-16 11:51:06 -0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,89 @@
+6.1 variáveis e constantes, quero exemplos.
+VARIÁVEIS: Um dado é variável quando possui um valor que pode variar durante a execução do algoritmo.
+Ex:
+var nome = Karla;
+
+CONSTANTES: Um dado é constante quando o seu valor não se modifica durante a execução do algoritmo.
+Ex:
+const VERDADEIRO = true;
+
+6.2 comandos de decisão, quero exemplos.
+Comandos de decisão servem para controlar o fluxo de execução do algoritmo. Por exemplo, o IF serve para que uma série de condições sejam satisfeitas para a execução de uma instrução ou bloco de instruções.
+Ex:
+
+6.3 loops, quero exemplos.
+While: Executa o comando enquanto a condição for verdadeira. 
+Ex:
+while ( c < 10) {
+          html +=  “Numero: “+c+” </br>
+          }
+	document.getElementById (“prova”).innerHTML = html;
+
+do while: É bem parecido com o while,  mas ele executa comandos pelo menos uma vez.
+        while (c < 10) {
+              
+
+For:  Ao iniciar o  for, ele faz o que está no início do laço. Geralmente se usa para inicializar algumas variáveis.
+Ex:
+6.4 funções, quero exemplos.
+Uma função é um conjunto de instruções que executa uma tarefa. Vai ser “chamada” em algum lugar no programa.
+
+function somar( x, y ) {
+   let total = x+y;
+   return total;
+}
+6.5 escopos, quero exemplos.
+Escopo é a acessibilidade de objetos, variáveis e funções em diferentes partes do código. 
+function ola (name) {
+    // Isto é um escopo
+  }
+  6.6 inputs e outputs, quero exemplos.
+  INPUT: Serve para receber dados do usuário
+  Ex: 
+  OUTPUT: Serve para exibir dados ao usuário
+  6.7 array, quero exemplos.
+  São objetos que contém diversos valores de um mesmo tipo. O array começa a contar do 0.
+  Ex:  let carros = [ “palio”, “corolla”, “ferrari”];
+               console.log ( carros )
+               6.8 objetos, quero exemplos.
+Enquanto o array é uma lista numerada que abre e fecha com colchetes [ ], o objeto é uma lista nomeada que abre e fecha com chaves {  }.
+
+Ex:  let carro =  {
+        nome: “fiat”;
+        modelo:”Palio”;
+        peso: “800kg”
+        };
+        6.9 (camelCase, PascalCase, kibab-case, snake_case).
+        Camel case: é feita sem espaços e cada letra começa com maiúscula, com exceção da primeira
+        PascalCase: é feita sem espaços e cada letra começa com maiúscula.
+        
+        Kibab-case: as palavras são em minúsculas e linkadas por hífen
+        
+        snake_case: as palavras são em minúsculas e linkadas pelo underscore.
+        
+        6.10 Diferença entre caminho absoluto e caminho relativo.
+        Caminho absoluto: aponta para a mesma localização em um sistema de arquivos, independentemente do diretório de trabalho atual.
+        
+        Caminho relativo: é baseado no diretório de trabalho atual. 
+        6.11 Diferença entre os operadores &&, ||, == e !=
+&& - significa “E”
+esse || significa “OU”
+
+!=  significa diferente
+== é sinal de igual mesmo.
+6.12 O que é EPER, e para que serve.
+EPER: ENTENDER, PLANEJAR, EXECUTAR E REVISAR
+
+ENTENDER: Entender o pedido do cliente
+PLANEJAR: Planejar o projeto antes de escrever qualquer código
+EXECUTAR: Executar a construção, escrever o código
+REVISAR: Fazer teste em produção
+
+6.13 Explique o que é Pilha e Fila, quero exemplos.
+PILHA: O primeiro que entra, é o último que sai.
+FILA: O primeiro que entra é o primeiro que sai
+6.14 Explique com suas palavras qual a diferença entre html, javascript e css.
+HTML: Linguagem de marcação de texto. É a base da página, sem ela não existiria o site.
+JAVASCRIPT: O javascript por sua vez, permite implementar itens complexos em páginas web.
+CSS: É o “embelezamento” do html. O CSS confere estilo à página. Alguns programadores comparam dizendo que ohtml é o esqueleto humano e o css o corpo humano(quer dizer, com olhos, boca,etc)  
+  
